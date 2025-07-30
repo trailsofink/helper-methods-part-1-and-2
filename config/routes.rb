cf2efd8d0fd8b8c9f0a56b50eea5f1f9c9d9ba8b
trailsofink <trailsofink@gmail.com>
2025-07-29 20:09:35 -0500

    updating url methods. updating urls to method_path urls in movies index.

--- config/routes.rb
+++ config/routes.rb
@@ -7,21 +7,21 @@ Rails.application.routes.draw do
 
   # CREATE
   # post("/movies", controller: "movies", action: "create" )
-  post "/movies" => "movies#create"
+  post "/movies" => "movies#create", as: :movies
   # get("/movies/new", controller: "movies", action: "new" )
-  get "/movies/new" => "movies#new"
+  get "/movies/new" => "movies#new", as: :new_movie
 
   # READ
   # get("/movies", controller: "movies", action: "index" )
   get "/movies" => "movies#index"
   # get("/movies/:id", controller: "movies", action: "show" )
-  get "/movies/:id" => "movies#show"
+  get "/movies/:id" => "movies#show", as: :movie # use in template as movie_path() or movie_url()
 
   # UPDATE
   # patch("/movies/:id", controller: "movies", action: "update" )
   patch "/movies/:id" => "movies#update"
   # get("/movies/:id/edit", controller: "movies", action: "edit" )
-  get "/movies/:id" => "movies#edit"
+  get "/movies/:id/edit" => "movies#edit", as: :edit_movie
 
   # DELETE
   # delete("/movies/:id", controller: "movies", action: "destroy" )
